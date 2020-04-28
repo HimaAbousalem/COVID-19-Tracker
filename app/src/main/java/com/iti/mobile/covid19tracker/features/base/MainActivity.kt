a558790b6b8652159eedf543f4f10fe4f036087b
ghadeerel-mahdy <GHADEER.mohamed1450@el-eng.menofia.edu.eg>
2020-04-29 00:47:37 +0200

    setup architecture

--- app/src/main/java/com/iti/mobile/covid19tracker/features/base/MainActivity.kt
+++ app/src/main/java/com/iti/mobile/covid19tracker/features/base/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.iti.mobile.covid19tracker
+package com.iti.mobile.covid19tracker.features.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iti.mobile.covid19tracker.R
 
 class MainActivity : AppCompatActivity() {
 
