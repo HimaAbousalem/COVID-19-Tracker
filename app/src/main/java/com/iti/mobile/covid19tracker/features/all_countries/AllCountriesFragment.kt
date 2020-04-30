8733c9149d0e9dd3b5420e45c80b8d0142d84b2d
ghadeerel-mahdy <GHADEER.mohamed1450@el-eng.menofia.edu.eg>
2020-04-30 17:09:46 +0200

    setup views

---
+++ app/src/main/java/com/iti/mobile/covid19tracker/features/all_countries/AllCountriesFragment.kt
@@ -0,0 +1,21 @@
+package com.iti.mobile.covid19tracker.features.all_countries
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.iti.mobile.covid19tracker.R
+
+class AllCountriesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_countries, container, false)
+    }
+
+
+}
